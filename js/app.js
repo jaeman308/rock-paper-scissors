59404df5ea6a4655b64e28850f9df0bd471bd266
jborruel308@me.com <jborruel308@me.com>
2024-07-25 19:57:18 -0700

    display result message

--- js/app.js
+++ js/app.js
@@ -14,24 +14,61 @@ const choices = ['rock', 'paper', 'scissors'];
 
 /*------------------------ Cached Element References ------------------------*/
 // 3) Reference to DOM element to display message
-const resultDispslayEl = document.querySelector ('#result-dsiplay')
+const resultDisplayEl = document.querySelector('#result-display')
     // 3 button DOM references for rock, paper, sicissors
 
 /*-------------------------------- Functions --------------------------------*/
 // handle generating random selections for the computer player
+// 4) handle player choice 
+const getPlayerChoice = (event) => {
+    playerChoice = event.target.id;
+}
+const getComputerChoice = () => {
+    const randomIndex = Math.floor (Math.random() * 
+    choices.length)
+    computerChoice = choices[randomIndex];
+}
 // compare player and computer choices check for a winner
-    // if/else comparison to all choices 
-        // rock beats scissors
-        // scissors beats paper
-        // paper beats rock 
-        // same options - its a tie
-
+// if/else comparison to all choices 
+// rock beats scissors
+// scissors beats paper
+// paper beats rock 
+// same options - its a tie
+const compare = () => {
+    if (playerChoice === computerChoice) {
+      msg = 'You tied!';
+    } else if (playerChoice === choices[0] && computerChoice === choices[2]) { 
+      // "rock" vs. "scissors"
+      msg = 'Congrats! You win!';
+    } else if (playerChoice === choices[1] && computerChoice === choices[0]) { 
+      // "paper" vs. "rock"
+      msg = 'Congrats! You win!';
+    } else if (playerChoice === choices[2] && computerChoice === choices[1]) { 
+      // "scissors" vs. "paper"
+      msg = 'Congrats! You win!';
+    } else {
+      msg = 'You lose! Try again?';
+    }
+    console.log(msg)
+}
+  
 //render a win/lose/tie message to the player 
-    // including player and comp choices in the msg
-    // clearly indicating who won/result 
+const render = () => {
+    resultDisplayEl.textContent = `You chose ${playerChoice} and the computer chose 
+    ${computerChoice}. ${msg}`;
+}
+// including player and comp choices in the msg
+// clearly indicating who won/result 
+
+const play = (event) => {
+    getPlayerChoice(event);
+    getComputerChoice();
+    compare();
+    render();
+};
 
 /*----------------------------- Event Listeners -----------------------------*/
 // 5)handle a plyaer clicking a button 
-document.querySelector('#rock').addEventListener('clic', play)
-document.querySelector('#paper').addEventListener('clic', play);
-document.querySelector('#scissors').addEventListener('clic', play);
+document.querySelector('#rock').addEventListener('click', play)
+document.querySelector('#paper').addEventListener('click', play);
+document.querySelector('#scissors').addEventListener('click', play);
