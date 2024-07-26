4cc612ea9e4eed39d5c32e3c71833dba8d104c2d
jborruel308@me.com <jborruel308@me.com>
2024-07-25 18:42:18 -0700

    set project files and skeleton code

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/*-------------------------------- Constants --------------------------------*/
+// 1) 3 user choices of "rock", "paper", or "siccors"
+/*-------------------------------- Variables --------------------------------*/
+// 2) Define our variables for state 
+    // The platers choice
+    // The comp choice
+    // The match result - win/lose/tie
+    // A result message- display the result/ who won 
+/*------------------------ Cached Element References ------------------------*/
+// 3) Reference to DOM element to display message
+    // 3 button DOM references for rock, paper, sicissors
+/*-------------------------------- Functions --------------------------------*/
+// handle generating random selections for the computer player
+// compare player and computer choices check for a winner
+    // if/else comparison to all choices 
+        // rock beats scissors
+        // scissors beats paper
+        // paper beats rock 
+        // same options - its a tie
+
+//render a win/lose/tie message to the player 
+    // including player and comp choices in the msg
+    // clearly indicating who won/result 
+/*----------------------------- Event Listeners -----------------------------*/
+// handle a plyaer clicking a button 
+
