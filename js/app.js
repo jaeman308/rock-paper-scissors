ff0a2881276482f03a0e3ea70b810feb6a7e3213
jborruel308@me.com <jborruel308@me.com>
2024-07-25 18:52:40 -0700

    add state variable and choice

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
 // 1) 3 user choices of "rock", "paper", or "siccors"
+const choices = ['rock', 'paper', 'scissors'];
+
 /*-------------------------------- Variables --------------------------------*/
-// 2) Define our variables for state 
-    // The platers choice
+// 2) Define our variables for state  (state is the status what changes do we need to keep track of)
+    // The players choice
+    let playerChoice;
     // The comp choice
+    let computerChoice;
     // The match result - win/lose/tie
     // A result message- display the result/ who won 
+    let msg;
+
 /*------------------------ Cached Element References ------------------------*/
 // 3) Reference to DOM element to display message
+const resultDispslayEl = document.querySelector ('#result-dsiplay')
     // 3 button DOM references for rock, paper, sicissors
+
 /*-------------------------------- Functions --------------------------------*/
 // handle generating random selections for the computer player
 // compare player and computer choices check for a winner
